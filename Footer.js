f447e1d5740592c81136386f7f700e3b8c3ba7b1
aayush036 <108410412+aayush036@users.noreply.github.com>
2024-03-12 22:48:29 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Footer(){
+    return(
+        <footer>
+            2021 Patels developement . All rights reserved.
+        </footer>
+    )
+}
---
+++ MainComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default function MainComponent(){
+    return(
+    <div>
+    <h1>
+    why im so excited to  learn react?
+</h1>
+<ol>
+    <li> I want a placement </li>
+    <li>secondly i want an intership </li>
+    <li>i dont want to be an average</li>
+</ol>
+</div>
+    )
+}
